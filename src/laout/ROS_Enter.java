1cd7ae3083275c6d762d1a936d2f6a7c723c525f
migoey <migoey@migoey-vm>
2023-01-13 21:28:24 +0900

    1234

--- src/laout/ROS_Enter.java
+++ src/laout/ROS_Enter.java
@@ -17,22 +17,20 @@ public class ROS_Enter extends JPanel {
 	protected static JTable table = null;
     
     public ROS_Enter() {
-    	JScrollPane scrollPane = new JScrollPane(table);
+		JScrollPane scrollPane = new JScrollPane(table);
+		Object[] roscolumnNames = (Object[]) ROS_JtableDATA.columnNames(); 
+		Object[][] rosData = (Object[][]) ROS_JtableDATA.roscrsData();  
+		//        System.out.println(Arrays.deepToString(rosData));
+		table = new JTable(rosData, roscolumnNames);
+		table.setFillsViewportHeight(true);
+		table.getColumnModel().getColumn(0).setMinWidth(240);
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(2400, 600));
+		table.setRowHeight(30);
+		table.setFont(new Font("Arial", Font. PLAIN, 16));
+		add(table.getTableHeader(), BorderLayout.PAGE_START);
+		add(table, BorderLayout.CENTER);
 
-    	 Object[] roscolumnNames = (Object[]) ROS_JtableDATA.columnNames(); 
-        Object[][] rosData = (Object[][]) ROS_JtableDATA.roscrsData();  
-//        System.out.println(Arrays.deepToString(rosData));
-        table = new JTable(rosData, roscolumnNames);
-        table.setFillsViewportHeight(true);
-        table.getColumnModel().getColumn(0).setMinWidth(240);
-        
-        setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(2400, 600));
-        table.setRowHeight(30);
-        table.setFont(new Font("Arial", Font. PLAIN, 16));
-        add(table.getTableHeader(), BorderLayout.PAGE_START);
-        add(table, BorderLayout.CENTER);
-        
         TableColumn column = null;
 	        for (int i = 0; i < 8; i++) {
 	            column = table.getColumnModel().getColumn(i);
@@ -42,25 +40,19 @@ public class ROS_Enter extends JPanel {
 	                column.setPreferredWidth(50);
 	            }
 	        }
-    
 table.addMouseListener(new MouseAdapter(){
     public void mousePressed(MouseEvent e){
-
     	 JTable table = (JTable)e.getSource();
         int row = table.rowAtPoint( e.getPoint() );
         int column = table.columnAtPoint( e.getPoint() );
-
         if (! table.isRowSelected(row))
 	        table.changeSelection(row, column, false, false);
 	        table.getModel().getValueAt(row, column);
 	        getCellValue = (String) table.getModel().getValueAt(row, column);
-
 	        ROS_AddArrayList.main(getCellValue,row,column);
     }
 });
-
     }
-
     private static void showFrame() {
         JPanel panel = new ROS_Enter();
         panel.setOpaque(true);
@@ -70,7 +62,6 @@ table.addMouseListener(new MouseAdapter(){
         frame.pack();
         frame.setVisible(true);
     }
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(ROS_Enter::showFrame);
     }
