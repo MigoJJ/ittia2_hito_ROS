ff15fff615a65f3e1fa5beb999dc7a727137ccc9
migowj <migowj@wj-vm>
2023-01-29 08:55:19 +0900

    1234

--- src/lgs_comm/GetRowNoAt.java
+++ src/lgs_comm/GetRowNoAt.java
@@ -8,19 +8,17 @@ import java.util.List;
 import laout.ROS_Enter;
 
 public class GetRowNoAt extends ROS_Enter{
+	public static String currentUsersHomeDir = System.getProperty("user.dir" );
+
 	public GetRowNoAt() throws IOException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public static String currentUsersHomeDir = System.getProperty("user.dir" );
 	
 	public static void getIndexRow(int row) throws IOException{
 		System.out.println(currentUsersHomeDir);
-
 		File_cdrw_proc pjc = new File_cdrw_proc();
 		int column = 0;
-				
 		for (int i = 0; i < 8; i++) {
 			String rcgetCellValue = (String) table.getModel().getValueAt(row, i);
 			if (i==0) {
